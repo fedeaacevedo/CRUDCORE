2e9e54fa0bc41a723849f02594da95950f15ea98
Federico <fedeaacevedo@gmail.com>
2022-06-03 19:52:56 -0300

    Controladores

---
+++ CRUDCORE/Controllers/MantenedorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CRUDCORE.Datos;
+using CRUDCORE.Models;
+
+namespace CRUDCORE.Controllers
+{
+    public class MantenedorController : Controller
+    {
+
+        ContactoDatos _ContactoDatos = new ContactoDatos();
+        public IActionResult Listar()
+        {
+            //La lista devuelve los contactos
+            var oLista = _ContactoDatos.Listar();
+            return View(oLista);
+        }
+
+        public IActionResult Guardar()
+        {
+            //SOLO DEVUELVE LA VISTA
+            return View();
+        }
+        public IActionResult Guardar()
+        {
+            //LO RECIBE Y GUARDA EN LA BASE DE DATOS
+            return View();
+        }
+    }
+}
